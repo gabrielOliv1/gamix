ea82adb78f8540f20668201d46d4e95df1aeab85
ViniciusONevesRodrigues <vinicius.oliveira.neves.rodrigues@gmail.com>
2023-06-10 19:17:35 -0300

    fix: Text out of the box

--- app/pages/creating/newProject/JS/ProjectJS.js
+++ app/pages/creating/newProject/JS/ProjectJS.js
@@ -1,6 +1,6 @@
 function removerTexto() {
     document.getElementById("rectangle_1").value = "";
-}
+  }
 function removerTexto2() {
-    document.getElementById("inrectangle_2").value = "";
+    document.getElementById("rectangle_2").value = "";
 }
