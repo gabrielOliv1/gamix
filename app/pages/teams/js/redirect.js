6c16f9627bd32b7299e518d946c2952014c8fa46
Lucas Christian <lucas.christian.programmer@gmail.com>
2023-06-20 15:38:47 -0300

    fix: pronto para lançar na web

--- app/pages/teams/js/redirect.js
+++ app/pages/teams/js/redirect.js
@@ -2,7 +2,7 @@ const links = document.querySelectorAll('.nav-icons i');
 
 links.forEach(link => {
   link.addEventListener('click', function(event) {
-    if (link.classList.contains('current-page')) {
+    if (link.classList.contains('current-page') && !link.classList.contains('ph-users-three')) {
       event.preventDefault(); // Bloqueia o redirecionamento
     }
   });
